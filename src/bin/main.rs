c9b148d39754fb76c22495a52a8a6b329a871709
Taryn Hill <taryn@phrohdoh.com>
2017-04-05 19:12:58 -0500

    lib: Remove prints and restrict public api to get_entry_headers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,15 @@
 extern crate mixer;
 
-use std::io::BufReader;
-use std::fs::File;
 use std::env;
 
 fn main() {
     match env::args().nth(1) {
         None => println!("Please supply a path to a mix file!"),
         Some(path) => {
-            let f = File::open(&path).expect(&format!("Failed to open {}", &path));
-            let br = BufReader::new(f);
-            mixer::read(br).expect(&format!("Failed to read {}", path));
+            let headers = mixer::get_entry_headers(path);
+            for hdr in headers {
+                println!("{:?}", hdr);
+            }
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 extern crate byteorder;
 use byteorder::{LittleEndian, ReadBytesExt};
 
-use std::io::{Read, Seek, SeekFrom};
+use std::io::{Read, Seek, SeekFrom, BufReader};
+use std::path::Path;
+use std::fs::File;
 
 struct MixHeader {
     num_files: u16,
     header_end_offset: u64,
 }
 
-struct EntryHeader {
+#[derive(Debug)]
+pub struct EntryHeader {
     hash: u32,
     offset: u32,
     len: u32,
 }
 
-pub fn read<T: Read + Seek>(mut data: T) -> Result<(), std::io::Error> {
+pub fn get_entry_headers<P: AsRef<Path> + std::fmt::Debug>(file_path: P) -> Vec<EntryHeader> {
+    let f = File::open(&file_path).unwrap();
+    let br = BufReader::new(f);
+    read(br).unwrap()
+}
+
+fn read<T: Read + Seek>(mut data: T) -> Result<Vec<EntryHeader>, std::io::Error> {
     let is_cnc_mix = data.read_u16::<LittleEndian>()? != 0;
-    println!("is_cnc_mix: {}", is_cnc_mix);
 
     let mut is_encrypted = false;
 
@@ -24,27 +32,19 @@ pub fn read<T: Read + Seek>(mut data: T) -> Result<(), std::io::Error> {
         is_encrypted = (data.read_u16::<LittleEndian>()? & 0x2) != 0;
     }
 
-    println!("is_encrypted: {}", is_encrypted);
-
-    let mh = if is_encrypted {
+    let mix_header = if is_encrypted {
         panic!("TODO: encrypted mix!");
     } else {
         let header_offset = if is_cnc_mix { 0 } else { 4 };
         read_header(&mut data, header_offset)?
     };
 
-    println!("num_files: {}", mh.num_files);
-    println!("header_end_offset: {}", mh.header_end_offset);
-
-    for i in 0..mh.num_files {
-        let eh = read_entry_header(&mut data)?;
-        println!("[{}] hash: {}", i, eh.hash);
-        println!("[{}] offset: {}", i, eh.offset);
-        println!("[{}] len: {}", i, eh.len);
-        println!("");
+    let mut entry_headers = Vec::new();
+    for i in 0..mix_header.num_files {
+        entry_headers.push(read_entry_header(&mut data)?);
     }
 
-    Ok(())
+    Ok(entry_headers)
 }
 
 fn read_header<T: Read + Seek>(data: &mut T, abs_offset: u64) -> Result<MixHeader, std::io::Error> {
